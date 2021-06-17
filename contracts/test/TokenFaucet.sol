0e567dfefef4b026f04735cce48c9aa3bfe85dc1
Roberto Cano <3525807+robercano@users.noreply.github.com>
2021-06-17 14:23:09 +0200

    Cleanup

--- contracts/test/TokenFaucet.sol
+++ contracts/test/TokenFaucet.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "../owner/Operator.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 
 /**
     Contract to provide with token funds to the caller
  */
-contract TokenFaucet is Operator {
+contract TokenFaucet is Context {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
