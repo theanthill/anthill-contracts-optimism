deb3956324a0a0dace95d7e8eea51c698fc4dbf7
Roberto Cano <3525807+robercano@users.noreply.github.com>
2021-06-18 18:35:24 +0200

    Cleanup

--- contracts/governance/Timelock.sol
+++ contracts/governance/Timelock.sol
@@ -4,10 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract Timelock is TimelockController {
-    address public admin;
-    address public pendingAdmin;
-    uint256 public delay;
-
     constructor(uint256 minDelay, address[] memory admins) TimelockController(minDelay, admins, admins) {
     }
 }
