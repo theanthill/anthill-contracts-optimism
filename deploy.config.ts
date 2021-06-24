ea20c78ea8c7c4ea70094f13a17521d76fe6436f
Roberto Cano <3525807+robercano@users.noreply.github.com>
2021-06-24 21:57:06 +0200

    Use configurable periods for governance contracts

--- deploy.config.ts
+++ deploy.config.ts
@@ -2,9 +2,21 @@
 // const ORACLE_START_DATE   = Math.round(Date.now() / 1000) + 1 * 60 * 120;
 // const TREASURY_START_DATE = Math.round(Date.now() / 1000) + 1 * 60 * 120;
 
+// [workerant] Set the final values before mainnet deployment
+
+// Start dates
 const POOL_START_DATE     = 1619777245; // 01/14/2021 @ 4:00am (UTC)
 const ORACLE_START_DATE   = 1619777245; // 01/19/2021 @ 4:00am (UTC)
 const TREASURY_START_DATE = 1619777245; // 01/21/2021 @ 4:00am (UTC)
+
+// Epoch periods
+const POOL_PERIOD = 600;       // 10 minutes
+const ORACLE_PERIOD = 86400;   // 1 Day
+const TREASURY_PERIOD = 86400; // 1 Day
+
+// Timelocks
+const TREASURY_TIMELOCK_PERIOD = 2*86400; // 2 Days
+const OPERATOR_TIMELOCK_PERIOD = 2*86400; // 2 Days
 
 // PancakeSwap Factory
 const UNI_FACTORY = '0x6725F303b657a9451d8BA641348b6761A6CC7a17';
@@ -16,8 +28,13 @@ const MAIN_NETWORKS = ['mainnet', 'local-mainnet'];
 
 module.exports = {
     POOL_START_DATE,
+    POOL_PERIOD,
     ORACLE_START_DATE,
+    ORACLE_PERIOD,
     TREASURY_START_DATE,
+    TREASURY_PERIOD,
+    TREASURY_TIMELOCK_PERIOD,
+    OPERATOR_TIMELOCK_PERIOD,
     UNI_FACTORY,
     LOCAL_NETWORKS,
     TEST_NETWORKS,
--- migrations/50_deploy_governance.js
+++ migrations/50_deploy_governance.js
@@ -1,9 +1,10 @@
 /**
  * Deploys all governance contracts
  */
-const {TREASURY_ACCOUNT, ADMIN_ACCOUNT, OPERATOR_ACCOUNT,
-       TEST_TREASURY_ACCOUNT, TEST_ADMIN_ACCOUNT, TEST_OPERATOR_ACCOUNT} = require('./migration-config');
-const {ORACLE_START_DATE, TREASURY_START_DATE, MAIN_NETWORKS} = require('../deploy.config.ts');
+const {TREASURY_ACCOUNT, OPERATOR_ACCOUNT,
+       TEST_TREASURY_ACCOUNT, TEST_OPERATOR_ACCOUNT} = require('./migration-config');
+const {ORACLE_START_DATE, TREASURY_START_DATE, MAIN_NETWORKS,
+       ORACLE_PERIOD, TREASURY_PERIOD, TREASURY_TIMELOCK_PERIOD, OPERATOR_TIMELOCK_PERIOD } = require('../deploy.config.ts');
 const {getPancakeFactory, getBUSD, getBandOracle} = require('./external-contracts');
 
 // ============ Contracts ============
@@ -17,33 +18,34 @@ const TreasuryTimelock = artifacts.require('TreasuryTimelock');
 const OperatorTimelock = artifacts.require('OperatorTimelock');
 const ContributionPool = artifacts.require('ContributionPool');
 
-const DAY = 86400;
-
 // ============ Main Migration ============
 async function migration(deployer, network, accounts) {
     const antToken = await AntToken.deployed();
+    const antBond = await AntBond.deployed();
     const antShare = await AntShare.deployed();
-    const pancakeswap = await getPancakeFactory(network);
+    const pancakeFactory = await getPancakeFactory(network);
     const bandOracle = await getBandOracle(network);
     const BUSD = await getBUSD(network);
     
     // Get the ANT/BUSD pair
-    const ANTBUSDPair = await pancakeswap.getPair(antToken.address, BUSD.address);
+    const ANTBUSDPair = await pancakeFactory.getPair(antToken.address, BUSD.address);
 
     // Deploy all governance contracts
     await deployer.deploy(Boardroom, antToken.address, antShare.address);
-    await deployer.deploy(Oracle, ANTBUSDPair, DAY, ORACLE_START_DATE, bandOracle.address);
+    await deployer.deploy(Oracle, ANTBUSDPair, ORACLE_PERIOD, ORACLE_START_DATE, bandOracle.address);
     await deployer.deploy(ContributionPool);
-    await deployer.deploy(Treasury, antToken.address, AntBond.address, AntShare.address, Oracle.address, Boardroom.address, ContributionPool.address, TREASURY_START_DATE);
+    await deployer.deploy(Treasury, antToken.address, antBond.address, antShare.address, 
+                                    Oracle.address, Boardroom.address, ContributionPool.address,
+                                    TREASURY_START_DATE, TREASURY_PERIOD);
 
     // Timelocks
     let adminAccount = network.includes(MAIN_NETWORKS) ? TREASURY_ACCOUNT : TEST_TREASURY_ACCOUNT;
     console.log(`Deploying ${TreasuryTimelock.contractName} for account Treasury (${adminAccount}) as both proposer and executor`);
-    await deployer.deploy(TreasuryTimelock, 2 * DAY, [adminAccount]);
+    await deployer.deploy(TreasuryTimelock, TREASURY_TIMELOCK_PERIOD, [adminAccount]);
 
     adminAccount = network.includes(MAIN_NETWORKS) ? OPERATOR_ACCOUNT : TEST_OPERATOR_ACCOUNT;
     console.log(`Deploying ${OperatorTimelock.contractName} for account Operator (${adminAccount}) as both proposer and executor`);
-    await deployer.deploy(OperatorTimelock, 2 * DAY, [adminAccount]);
+    await deployer.deploy(OperatorTimelock, OPERATOR_TIMELOCK_PERIOD, [adminAccount]);
 }
 
 module.exports = migration;
